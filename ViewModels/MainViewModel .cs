42554451c1a586a7e2d7bb3d6015cb3e3e94a1cf
anass driate <driateanass@gmail.com>
2025-01-07 16:11:51 +0100

    add log text box and diff meas calculation

--- ViewModels/MainViewModel .cs
+++ ViewModels/MainViewModel .cs
@@ -16,6 +16,7 @@ using System.Xml.Linq;
 using WpfApp1.Services;
 using System.Windows.Media.Animation;
 using System.Text.RegularExpressions;
+using System.Runtime.Remoting.Contexts;
 
 
 namespace WpfApp1.ViewModels
@@ -25,6 +26,7 @@ namespace WpfApp1.ViewModels
         private int _goodProductCount;
         private int _badProductCount;
         private string _leftPanelText; // New property for LeftPanelText
+        private string _logText;
 
         private Thread _workerThread; // Thread reference
         private bool _isRunning;      // Status flag
@@ -46,6 +48,7 @@ namespace WpfApp1.ViewModels
             GoodProductCount = 0;
             BadProductCount = 0;
             LeftPanelText = "Welcome to the Quality Control App";
+            LogText = "...";
 
             // Commands
             ResetCommand = new RelayCommand(ResetCounters);
@@ -85,6 +88,17 @@ namespace WpfApp1.ViewModels
             {
                 _leftPanelText = value;
                 OnPropertyChanged(nameof(LeftPanelText)); // Notify UI when this changes
+            }
+        }
+
+        // New property for LogText bound to the TextBox
+        public string LogText
+        {
+            get { return _logText; }
+            set
+            {
+                _logText = value;
+                OnPropertyChanged(nameof(LogText)); // Notify UI when this changes
             }
         }
 
@@ -153,6 +167,7 @@ namespace WpfApp1.ViewModels
                         //Log.Information("No URL read or invalid URL, Dead tag");
                         continue; // Skip to the next iteration if the URL is invalid
                     }
+                    LogText = url + "          Diff Meas " + calcdiffmeas(url);
                     // Check if the product is closed
                     if (Utilities.CheckIfClosed(url))
                     {
@@ -179,6 +194,47 @@ namespace WpfApp1.ViewModels
             Log.Information("Thread stopped.");
         }
 
+        private int calcdiffmeas(string url)
+        {
+
+            string CTT_hex = "";
+            string[] dat = url.Split('x');
+            CTT_hex = "" + dat[dat.Length - 2][8] + dat[dat.Length - 2][9] + dat[dat.Length - 2][6] + dat[dat.Length - 2][7] + dat[dat.Length - 2][4] + dat[dat.Length - 2][5] + dat[dat.Length - 2][2] + dat[dat.Length - 2][3] + dat[dat.Length - 2][0] + dat[dat.Length - 2][1];
+            StringBuilder cttb = new StringBuilder(CTT_hex);
+            if (cttb[8] == 'C')
+                cttb[8] = '3';
+            if (cttb[9] == 'C')
+                cttb[9] = '3';
+            if (cttb[8] == 'O')
+                cttb[8] = 'F';
+            if (cttb[9] == 'O')
+                cttb[9] = 'F';
+            // ERROR can happens if ctt contains 'I'
+            // if ctt contains 'I' replace it with 3 closed
+            if (cttb[9] == 'I')
+                cttb[9] = '9';
+            CTT_hex = cttb.ToString();
+            // ----------------------------------------------------------
+
+
+
+
+            //converting the ctt to binary and calculating signed diff_meas
+            // ----------------------------------------------------------
+            string diff_meas = "";
+            BitArray CTT_Binary;
+            byte[] ctt = Enumerable.Range(0, CTT_hex.Length).Where(x => x % 2 == 0).Select(x => Convert.ToByte(CTT_hex.Substring(x, 2), 16)).ToArray();
+            CTT_Binary = new BitArray(ctt, 40);
+            //for (int i = 0; i < 40; i++)
+            //    Console.WriteLine(CTT_Binary[i] ? "1" : "0");
+            diff_meas = (CTT_Binary[19] ? "1" : "0") + (CTT_Binary[20] ? "1" : "0") + (CTT_Binary[21] ? "1" : "0") + (CTT_Binary[22] ? "1" : "0") + (CTT_Binary[23] ? "1" : "0")
+                                  + (CTT_Binary[8] ? "1" : "0") + (CTT_Binary[9] ? "1" : "0") + (CTT_Binary[10] ? "1" : "0") + (CTT_Binary[11] ? "1" : "0") + (CTT_Binary[12] ? "1" : "0") + (CTT_Binary[13] ? "1" : "0");
+            int sig_diff_mea = (CTT_Binary[18] ? "1" : "0") == "1" ? -Math.Abs(Convert.ToInt32(diff_meas, 2)) : Convert.ToInt32(diff_meas, 2);
+            Log.Debug("Diff_Meas : " + sig_diff_mea);
+            return sig_diff_mea;
+            // ----------------------------------------------------------
+        }
+
         // Reset counters for products
         private void ResetCounters(object obj)
         {
@@ -186,6 +242,7 @@ namespace WpfApp1.ViewModels
             BadProductCount = 0;
             GoodProductCount = 0;
             LeftPanelText = string.Empty;
+            LogText = string.Empty;
 
         }
         private void OpenAbout(object obj)
